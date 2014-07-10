dc4c83c85c1aaf1f29b9e192edbfd441edb2f677
pengyucheng <pengyucheng@192.168.81.107>
2014-07-10 19:48:11 +0800

    可以正常运行

--- M2Books/src/main/java/com/cn/hsg/dao/NotifDao.java
+++ M2Books/src/main/java/com/cn/hsg/dao/NotifDao.java
@@ -1,8 +1,14 @@
 package com.cn.hsg.dao;
 
+import javax.sql.DataSource;
+
 import org.nutz.dao.Dao;
+import org.nutz.dao.impl.NutDao;
+import org.nutz.ioc.Ioc;
+import org.nutz.ioc.impl.NutIoc;
 import org.nutz.ioc.loader.annotation.Inject;
 import org.nutz.ioc.loader.annotation.IocBean;
+import org.nutz.ioc.loader.json.JsonLoader;
 import org.nutz.log.Log;
 import org.nutz.log.Logs;
 
@@ -14,11 +20,16 @@ import com.cn.hsg.pojo.Notification;
 public class NotifDao {
 
 	private static final Log log = Logs.get();
-	@Inject
-	private Dao dao;
 	
 	//重生时，添加关系表id:LINKID , Content:通知内容
 	public boolean insertT(Notification notif) {
+		JsonLoader js = new JsonLoader("ioc/dao.js");
+    	//将配置信息保存到dao.xml,并存放于src文件夹下
+    	Ioc ioc = new NutIoc(js);
+    	DataSource ds = ioc.get(DataSource.class);
+    	Dao dao = new NutDao(ds); //如果已经定义了dao,那么改成dao =
+
+    	ioc.get(Dao.class);
 		log.info("新建通知消息："+notif.getLinkID());
 		boolean suc = false;
 		Object tt = dao.insert(notif);
