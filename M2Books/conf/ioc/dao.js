25c73f39c0183f0b6470b93e548263ae8a5d0bb1
huos3203 <724987481@qq.com>
2014-07-10 16:33:38 +0800

    完善pom.xml依赖包 配置ioc依赖支持，连接本地数据库，并新建表ebooks

---
+++ M2Books/conf/ioc/dao.js
@@ -0,0 +1,78 @@
+//var ioc = {
+//	dataSource:{
+//		type:"com.alibaba.druid.pool.DruidDataSource",
+//		fields:{
+//			driverClassName:"org.h2.Driver",
+//			url:"jdbc:h2:nutz"
+//		},
+//		events : {
+//			depose : "close"
+//		}
+//	},
+//	
+//	dao : {
+//		type : "org.nutz.dao.impl.NutDao",
+//		args : [{refer:"dataSource"}]
+//	}	
+//};
+
+/**
+ * 	远程服务器操作SqlServer数据库
+ **/
+//var ioc = {
+//		dataSource : {
+//			type:"org.apache.commons.dbcp.BasicDataSource",
+//			
+//			fields:{
+//				driverClassName:"com.microsoft.sqlserver.jdbc.SQLServerDriver",
+//				url:"jdbc:sqlserver://192.168.86.31:1433;DatabaseName = fileout",  //发布:db.pyc.com.cn   //测试：192.168.86.31
+//				username : 'protect',
+//				password : 'admin@pyc.bj'
+//			},
+//			events : {
+//					depose : "close"
+//			}
+//		},
+//		dao : {
+//			type : "org.nutz.dao.impl.NutDao",
+//			args : [{refer:"dataSource"}]
+//		},
+//		
+//		userTokenDao : {
+//			type : "com.test.apple.dao.UserTokenDao",
+////			args : [{refer:"dao"}],
+//			fields : {
+//             dao : {refer : 'dao'}    // 指向容器里另外一个对象
+//			}
+//		},
+//		
+//		notifRelaDao : {
+//			type : "com.test.apple.dao.NotifRelaDao",
+//			fields : {
+//				dao : {refer : 'dao'}
+//			}
+//			
+//		}
+//		
+//};
+
+var ioc = {
+		dataSource : {
+			type:"org.apache.commons.dbcp.BasicDataSource",
+			
+			fields:{
+				driverClassName:"com.mysql.jdbc.Driver",
+				url:"jdbc:mysql://127.0.0.1:3306/ebooks?useUnicode=true&characterEncoding=utf-8",
+				username : 'root',
+				password : ''
+			},
+			events : {
+					depose : "close"
+			}
+		},
+		dao : {
+			type : "org.nutz.dao.impl.NutDao",
+			args : [{refer:"dataSource"}]
+		}
+		
+};
--- M2Books/src/main/java/com/cn/hsg/App.java
+++ M2Books/src/main/java/com/cn/hsg/App.java
@@ -1,5 +1,16 @@
 package com.cn.hsg;
 
+import javax.sql.DataSource;
+
+import org.nutz.dao.Dao;
+import org.nutz.dao.impl.NutDao;
+import org.nutz.ioc.Ioc;
+import org.nutz.ioc.impl.NutIoc;
+import org.nutz.ioc.loader.json.JsonLoader;
+import org.nutz.ioc.loader.xml.XmlIocLoader;
+
+import com.cn.hsg.pojo.Books;
+
 /**
  * Hello world!
  *
@@ -8,6 +19,17 @@ public class App
 {
     public static void main( String[] args )
     {
+    	JsonLoader js = new JsonLoader("ioc/dao.js");
+    	//将配置信息保存到dao.xml,并存放于src文件夹下
+    	Ioc ioc = new NutIoc(js);
+    	DataSource ds = ioc.get(DataSource.class);
+    	Dao dao = new NutDao(ds); //如果已经定义了dao,那么改成dao =
+
+    	ioc.get(Dao.class);
+    	dao.create(Books.class, true);
+//    	Books books = new Books();
+//    	dao.insert(books);
+    	ioc.depose(); //关闭Ioc容器
         System.out.println( "Hello World!" );
     }
 }
